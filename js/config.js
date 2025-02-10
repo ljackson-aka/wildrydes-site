75a12ae48611aadfecfa956da97d5a7b2df48ac6
ljackson-aka <63591083+ljackson-aka@users.noreply.github.com>
2025-02-10 08:39:21 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_umVBDPgTC', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '46tobi7s6o0uao20epg00n0oo0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
